00a0207bec7adbaecbef4b48f610988af8d8db77
Kanaru Nakahira <kanaru2917@gmail.com>
2025-04-27 23:52:05 +0900

    UIの調整

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ export const game = {
   State:   ["","","","","","","","",""], // マス目の状態管理用の変数。
   currentPlayer: true, // 入力待ちのプレイヤーを表す変数。trueの場合はX, falseの場合はOのターン。
   Over:    false, // ゲーム終了判定フラグ。
-  turnDisplay:  document.getElementById("turn-display"), // 入力待ちのプレイヤーを表示するdiv要素をグローバルに保持。
+  turnDisplay:  document.getElementsByClassName("turn-display")[0], // 入力待ちのプレイヤーを表示するdiv要素をグローバルに保持。
   boardCells:   document.querySelectorAll(".cell"), // ページ内の全ての.cell要素を取得してグローバル変数boardCells(= export let boardCells)にキャッシュしている。
   // 勝利した場合のindexパターン。
   winningPatterns: [ 
